ed8c91f21e28e6830f433328c6943aefb3128b4f
ccm24 <113595661+ccm24@users.noreply.github.com>
2023-10-26 19:47:31 +0200

    tarjetas

--- src/components/Slider/Article.js
+++ src/components/Slider/Article.js
@@ -1,13 +1,22 @@
 import './Article.css';
-import { VtmnTag } from '@vtmn/react';
+import { VtmnPrice, VtmnRating, VtmnTag } from '@vtmn/react';
 
-function Article({price, imgArticles, title, description}) {
+function Article({price, imgArticles, title, description,num}) {
   return (
     <article className="tarjeta">
-      <VtmnTag>{price}</VtmnTag>
-      <img src={imgArticles} className="imgArticles" alt="Articulo" />
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <div  className='cajaPrecio'>
+        <VtmnTag className='precio'>
+          <VtmnPrice>{price}</VtmnPrice> 
+        </VtmnTag>
+      </div>
+      <div className='caja'>
+        <img src={imgArticles} className="imgArticles" alt="Articulo" />
+      </div> 
+      <div className='cajaTexto'>
+        <h3>{title}</h3>
+        <p className='descripcion'>{description}</p> 
+        <VtmnRating size="small" value={num}></VtmnRating>
+      </div>
     </article>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,7 +6,13 @@ function Slider() {
     <section className='slider'>
         <h2 className='sectionTitle'>Top ventas cerca de ti</h2>
         <div className='tarjetas'>
-            <Article price="20€" title="KALENJI" imgArticles="https://contents.mediadecathlon.com/p1975311/k$33d2d8a1d90630ff128023783122a820/sq/pantalon-running-transpirable-mujer-dry-negro.jpg?f=3000x3000" description="heojfdnlczpksñd zxl" />
+            <Article 
+              price="20€" 
+              title="KALENJI" 
+              imgArticles="https://contents.mediadecathlon.com/p1975311/k$33d2d8a1d90630ff128023783122a820/sq/pantalon-running-transpirable-mujer-dry-negro.jpg?f=3000x3000" 
+              description="Pantalón jogger fitness recto algodón Mujer Domyos negro" 
+              num="3" 
+            />
             <Article price="20€"/>
             <Article price="20€"/>
             <Article price="20€"/>   
