d5c58006855624f0a4fc233e5b9c336bf9c3a3ab
ccm24 <ccmm0024@gmail.com>
2023-10-27 11:42:22 +0200

    historia 1

--- src/components/Slider/Article.js
+++ src/components/Slider/Article.js
@@ -1,22 +1,49 @@
 import './Article.css';
 import { VtmnPrice, VtmnRating, VtmnTag } from '@vtmn/react';
+import { useEffect, useState } from "react";
+
 
 function Article({price, imgArticles, title, description,num}) {
+  let[products,setProducts] =useState([]);
+
+  useEffect(
+      () => {
+          const options = {
+              method:'GET',
+          };
+
+          fetch('http://localhost:3000/products')
+              .then(response => response.json())
+              .then(data => setProducts(data))
+              .catch(err => console.error(err));
+      },
+      []
+  );
+
   return (
-    <article className="tarjeta">
-      <div  className='cajaPrecio'>
-        <VtmnTag className='precio'>
-          <VtmnPrice>{price}</VtmnPrice> 
-        </VtmnTag>
-      </div>
-      <div className='caja'>
-        <img src={imgArticles} className="imgArticles" alt="Articulo" />
-      </div> 
-      <div className='cajaTexto'>
-        <h3>{title}</h3>
-        <p className='descripcion'>{description}</p> 
-        <VtmnRating size="small" value={num}></VtmnRating>
-      </div>
+    <article  className='carousel'>
+      {
+        products.slice(15).map(product =>
+          <div>
+            <div className="tarjeta">
+              <div  className='cajaPrecio'>
+                <VtmnTag className='precio'>
+                  <VtmnPrice>{product.precio}</VtmnPrice> 
+                </VtmnTag>
+              </div>  
+              <div className='caja'>
+                <img src={product.imagen} className="imgArticles" alt="Articulo" />
+              </div> 
+              <div className='cajaTexto'>
+                <h3>{product.marca}</h3>
+                <p className='descripcion'>{product.nombre}</p> 
+                <VtmnRating size="small"></VtmnRating>
+              </div>
+            </div>
+          </div>
+            
+        )
+      }
     </article>
   );
 }
